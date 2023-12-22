a4e8f332f7c20f19c276c05b8e51544c45500d01
eugene <eugene@gmail.com>
2023-12-22 13:36:17 +0530

    dev version 1

---
+++ packer/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+systemctl restart sshd.service
+
+yum install httpd php -y
+
+cp -r /tmp/website/* /var/www/html/
+chown apache:apache /var/www/html/*
+
+systemctl restart httpd.service php-fpm.service
+systemctl enable httpd.service php-fpm.service
